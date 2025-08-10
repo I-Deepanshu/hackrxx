2ab907c1667610c9454ea6f74dc4819712a30105
KARTIK GARG <125589036+Kartikgarg74@users.noreply.github.com>
2025-08-10 19:18:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "fastapi",
-        "uvicorn",
-        "pydantic",
-        "uvicorn[standard]",
-        "python-dotenv",
-        "requests",
-        "pinecone-client",
-        "python-docx",
-        "PyMuPDF",
-        "pydantic",
-        "pydantic-settings",
-        "pytest",
-        "groq",
-        "sqlalchemy",
-        "psycopg2-binary",
-        "alembic",
-        "tiktoken"
+        line.strip()
+        for line in open("requirements.txt").readlines()
     ],
 )
